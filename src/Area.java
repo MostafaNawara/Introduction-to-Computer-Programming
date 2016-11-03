263c0612e39c3e42656e1fe1d521ee55700124e7
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-03 02:03:46 +0200

    Init Java App

---
+++ src/Area.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Area {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Get Area Calc");
+
+		System.out.println("Enter the height:");
+		
+		Scanner input = new Scanner(System.in);
+		
+		input.nextLine();
+
+		System.out.println("Enter the width:");
+
+		input.nextLine();
+
+	}
+
+}
