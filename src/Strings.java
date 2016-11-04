6144088c366858618095417294f9465011533676
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-04 16:40:14 +0200

    Add String Class

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+
+public class Strings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Some over / the vast ocean \\ 'there must' be a land \nwhere my thoughts / will meet the distant shorte ...\" \ncomplained a \"lonley and loging\" singer");
+
+	}
+
+}
