76f74fd499ffd79d4101b1d44c24b302bed8acf7
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-05 02:18:08 +0200

    Add TriangleCheck Class && FlowChart

---
+++ src/TriangleCheck.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class TriangleCheck {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Auto-generated method stub
+		Scanner inpScanner = new Scanner(System.in);
+
+		System.out.println("Enter The Triangle height:");
+		
+		int a = inpScanner.nextInt();
+		int b = inpScanner.nextInt();
+		int c = inpScanner.nextInt();
+		
+		if ((a + b > c) && (a + c > b) && (b + c > a)) {
+			System.out.println("It's a Triangle");
+		}else{
+			System.out.println("Not a Triangle");
+		}
+
+	}
+
+}
