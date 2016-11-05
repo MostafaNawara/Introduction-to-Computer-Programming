c1736838216672793d505b3889361c6065908ab7
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-05 05:09:15 +0200

    Add Sorrting class

---
+++ src/Sorting.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+
+public class Sorting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//  Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter three floating-point numbers");
+		
+		// Get numbers 
+		float a = input.nextFloat();
+		float b = input.nextFloat();
+		float c = input.nextFloat();
+		
+		if (a < b && a < c) {
+			System.out.println(a);
+
+			if (b < c) {
+				System.out.println(b);
+				System.out.println(c);
+			} else {
+				System.out.println(c);
+				System.out.println(b);
+			}
+		}else if (b < a && b < c){
+			System.out.println(b);
+
+			if (a < c) {
+				System.out.println(a);
+				System.out.println(c);
+			} else {
+				System.out.println(c);
+				System.out.println(a);
+			}
+		}else{
+			System.out.println(c);
+
+			if (a < b) {
+				System.out.println(a);
+				System.out.println(b);
+			} else {
+				System.out.println(b);
+				System.out.println(a);
+			}
+		}
+	}
+
+}
