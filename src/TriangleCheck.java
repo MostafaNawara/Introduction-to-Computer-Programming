9b7abfb13d41a57e426d041c865431a2e849db75
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-05 02:25:01 +0200

    use or opritor TriangleCheck class

--- src/TriangleCheck.java
+++ src/TriangleCheck.java
@@ -16,11 +16,19 @@ public class TriangleCheck {
 		int b = inpScanner.nextInt();
 		int c = inpScanner.nextInt();
 		
-		if ((a + b > c) && (a + c > b) && (b + c > a)) {
+/*		if ((a + b > c) && (a + c > b) && (b + c > a)) {
 			System.out.println("It's a Triangle");
 		}else{
 			System.out.println("Not a Triangle");
+		}*/
+		
+		if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+			System.out.println("Not a Triangle");
+		}else{
+			System.out.println("It's a Triangle");
+
 		}
+		
 
 	}
 
