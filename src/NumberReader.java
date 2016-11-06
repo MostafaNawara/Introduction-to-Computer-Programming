0b1ae300f3f13fb7b45cd7eab62f15b858181fc1
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-07 01:56:25 +0200

    Done Number reader

--- src/NumberReader.java
+++ src/NumberReader.java
@@ -12,22 +12,35 @@ public class NumberReader {
 		System.out.println("Enter a number");
 
 
-		int x = input.nextInt();	
+		int x = input.nextInt();
+		int steps = 0;
 		
 		
 		if (x <= 1) {
 			System.out.println("\"Error\" Add number more than 1");
 		}else {
-			while ( x == 1) {
+			while ( x != 1) {
 				if (x % 2 == 0) {
+
 					x = x / 2;
+					
+					// Update the steps value
+					steps = steps + 1;
+					
+					System.out.println(x);
 				} else {
+
 					x = (x * 3) + 1;
-				}			
+					
+					// Update the steps value
+					steps = steps + 1;
+					
+					System.out.println(x);
+				}
 			}
-	
+			
+			System.out.println("Number of steps: " + steps);
 		}
-		
 
 	}
 
