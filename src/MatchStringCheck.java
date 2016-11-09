94a69f8eb14e71bdc85ab851ad750546ae84769c
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-09 03:29:44 +0200

    Done MatchStringCheck :D

--- src/MatchStringCheck.java
+++ src/MatchStringCheck.java
@@ -16,36 +16,30 @@ public class MatchStringCheck {
 		String Second = input.nextLine();
 		
 		int firstLength = First.length();
-		int secondLength = Second.length();
-		
+		int secondLength = Second.length()-firstLength+1;
+
 		int matched = 0;
 		
-		// System.out.println(fristLength);
 
 		for (int i = 0; i < secondLength ; i++) {
-			char x = First.charAt(i);
-			char y = Second.charAt(i);
+			for (int j = 0; j < firstLength; j++) {
+				if (First.charAt(j) == Second.charAt(i+j)) {
+					if (j == firstLength-1) {
+						matched = matched + 1;						
+					}
+				}else {
+					break;
+				}
+			}			
 		}
-		
+
+
 		if (matched != 0) {
 			System.out.println("Number of matching = " + matched);
 		}else {
 			System.out.println("Not found matched words");			
 		}
 
-/*
- *
- * 	6) Write a program that returns number of occurrences of a string in another string.
- *	E.g.
- *	Input:
- *	First String: the
- *	Second String: the students are working hard in the faculty of Engineering because they
- *	love it
- *	Output:
- *	3
- *	Note: You should use nested loops. Don’t use some methods like indexOf or substring
- *
- */
 
 	}
 
