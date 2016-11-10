270560690482c32825badf5db093cd27278a9814
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-10 03:09:45 +0200

    Add array class

---
+++ src/Arrays.java
@@ -0,0 +1,20 @@
+
+public class Arrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int [] x = {1, 2, 3, 4, 5, 6};
+		System.out.println(x[2]);
+		
+		// array with max items
+		double [] y = new double[3];
+		
+		y[0] = 2.2;
+		y[1] = 3.2;
+		y[2] = 4.4;
+	}
+
+}
