c971f3c348aa9ebebd960787f5a69e1997a0f248
Mostafa Nawara <nawaragfx@gmail.com>
2016-11-18 14:23:58 +0200

    update union array

--- src/ArrayUnionValue.java
+++ src/ArrayUnionValue.java
@@ -27,11 +27,11 @@ public class ArrayUnionValue {
 
 	
 		// 2nd Array
-		System.out.println("Enter First Array size: ");		
+		System.out.println("Enter Second Array size: ");		
 		int secondArraySize = input.nextInt();
 		input.nextLine();
 
-		System.out.println("Enter first array " + secondArraySize + " elements:");		
+		System.out.println("Enter Second array " + secondArraySize + " elements:");		
 
 		// init the array
 		int [] secondArray = new int[secondArraySize];
@@ -45,69 +45,38 @@ public class ArrayUnionValue {
 		// Get the output
 
 		// init the array
-		int [] UnionArray = {};
-		int [] IntersectionArray = {};
-
+		int [] UnionArray = new int [firstArraySize + secondArraySize];
+		int [] IntersectionArray = new int [firstArraySize + secondArraySize];
+		
+		int UnionArraySize = 0;
+		int IntersectionArraySize = 0;
+		
+		
 		for (int i = 0; i < firstArraySize; i++) {
 			for (int j = 0; j < secondArraySize; j++) {
 				
+				if (firstArray[i] == secondArray[j]){
+					IntersectionArray[IntersectionArraySize] = firstArray[i];
+					IntersectionArraySize++;
+				}else {
+					UnionArray[UnionArraySize] = firstArray[i];
+					UnionArraySize++;					
+				}
+				
 			}
 		}
-		
-		System.out.println(UnionArray);
-		System.out.println(IntersectionArray);
 
-/*
-		Write Java program that given two arrays of integers, prints the union array and the intersection array.
-		Assume that the input arrays do not consist of duplicates.
-		For example
-		Array1: 12, 32, 14, 35, 89, 16, 120
-		Array2: 9, 12, 8, 17, 120, 35, 36
-		Union (Array1, Array2): 12, 32, 14, 35, 89, 16, 120, 9, 8, 17, 36
-		Intersection (Array1, Array2): 12, 35, 120
-		Input:
-		Enter first array size: 7
-		Enter first array elements:
-		12
-		32
-		14
-		35
-		89
-		16
-		120
-		Enter second array size: 7
-		Enter second array elements:
-		9
-		12
-		8
-		17
-		120
-		35
-		36
-		Page 5
-		Output:
-		Union:
-		12
-		32
-		14
-		35
-		89
-		16
-		120
-		9
-		8
-		17
-		36
-		Intersection:
-		12
-		35
-		120
-		Note: You should use loops here.
+		// Print the result
+		System.out.println("Union (Array1, Array2):");
+		for (int i = 0; i < UnionArray.length; i++) {
+			System.out.print(UnionArray[i] + ",");
+		}
 		
-*/
-		
-			
-
+		System.out.println("");
+		System.out.println("Intersection (Array1, Array2):");
+		for (int i = 0; i < IntersectionArray.length; i++) {
+			System.out.print(IntersectionArray[i] + ",");
+		}
 	}
 }
 
